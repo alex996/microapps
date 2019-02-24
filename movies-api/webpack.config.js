5a84a14971a6da16350c2e0523c5d9d5922eff95
Alex <elitecodeartist@gmail.com>
2019-02-24 12:32:51 -0500

    [movies-api] Configure source map

--- movies-api/webpack.config.js
+++ movies-api/webpack.config.js
@@ -1,13 +1,19 @@
 const nodeExternals = require('webpack-node-externals')
 const NodemonPlugin = require('nodemon-webpack-plugin')
+const { BannerPlugin } = require('webpack')
 
 module.exports = {
   target: 'node',
-  devtool: 'cheap-source-map',
+  devtool: 'cheap-module-source-map',
   externals: [
     nodeExternals()
   ],
   plugins: [
-    new NodemonPlugin()
+    new NodemonPlugin(),
+    new BannerPlugin({
+      raw: true,
+      entryOnly: true,
+      banner: `require('source-map-support').install();`
+    })
   ]
 }
