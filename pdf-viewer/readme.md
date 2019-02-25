515d4287154242b3ecdf786a1638602197c799dd
Alex <elitecodeartist@gmail.com>
2019-02-25 00:06:52 -0500

    [pdf-viewer] Add filename hash

--- pdf-viewer/readme.md
+++ pdf-viewer/readme.md
@@ -9,4 +9,4 @@
 
 ## TODO
 
-- code splitting + bundle hash
+- code splitting
--- pdf-viewer/webpack.config.js
+++ pdf-viewer/webpack.config.js
@@ -2,37 +2,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = (env, { mode }) => ({
-  devtool: 'cheap-module-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, { mode }) => {
+  const devMode = mode === 'development'
+
+  return {
+    devtool: 'cheap-module-source-map',
+    output: {
+      filename: devMode ? '[name].js' : '[name].[contenthash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ]
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: 'node_modules/pdfjs-dist/cmaps/',
+          to: 'cmaps/'
+        }
+      ]),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'node_modules/pdfjs-dist/cmaps/',
-        to: 'cmaps/'
-      }
-    ]),
-    new MiniCssExtractPlugin()
-  ]
-})
+  }
+}
