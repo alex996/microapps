84b8db3f7e85a16e666f9e165920277246631d4a
Alex <alexnezhynsky@gmail.com>
2019-02-27 21:22:16 -0500

    [todo-list] Init commit

---
+++ todo-list/readme.md
@@ -0,0 +1,7 @@
+# Todo List
+
+To-do list app using React Context API (16.3) and `useReducer` hook (16.8).
+
+## Motivation
+
+Demonstrate a global context-based store with a Redux-like action system.
---
+++ todo-list/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+}
