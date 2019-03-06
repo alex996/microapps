f06f870d40a8c34f0c44a8651aef6ad40ed260ee
Alex <alexnezhynsky@gmail.com>
2019-03-05 22:59:46 -0500

    [movies-api] Return proper status code on err

--- movies-api/src/middleware/fallback.js
+++ movies-api/src/middleware/fallback.js
@@ -1,11 +1,24 @@
 import { NotFound } from '../errors'
 
+const getStatusCode = err =>
+  err.status
+    ? err.status
+    : err.name === 'ValidationError'
+      ? 422
+      : err.name === 'MongoError'
+        ? 400
+        : 500
+
 export const notFound = (req, res, next) => {
   next(new NotFound())
 }
 
 export const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500).json({
+  const status = getStatusCode(err)
+
+  status === 500 && console.error(err)
+
+  res.status(status).json({
     message: err.message || 'Internal Server Error'
   })
 }
