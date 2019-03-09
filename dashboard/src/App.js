8cbd689ba31e65151cf19312cd53a0214334e78c
Alex <alexnezhynsky@gmail.com>
2019-03-09 01:17:00 -0500

    [dashboard] Set up emotion

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  background: #eaeaea;
+  padding: 20px;
+`
+
+const App = props => (
+  <Container>App</Container>
+)
+
+export default App
