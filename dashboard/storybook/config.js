73d758267bbf4be221ac82f55985dbfd2a3d89f9
Alex <alexnezhynsky@gmail.com>
2019-03-09 13:40:25 -0500

    [dashboard] Move JSS insertion point

--- dashboard/storybook/config.js
+++ dashboard/storybook/config.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'emotion-theming'
-import { CssBaseline } from '@material-ui/core'
+import { jssPreset, CssBaseline } from '@material-ui/core'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
 import theme from '../src/theme'
+
+const jss = create({ ...jssPreset(), insertionPoint: 'jss-insertion-point' })
+
+addDecorator(story => (
+  <JssProvider jss={jss}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {story()}
+    </ThemeProvider>
+  </JssProvider>
+))
 
 const loadStories = () => {
   const req = require.context('../src/components', true, /\.stories\.js$/)
   req.keys().forEach(filename => req(filename))
 }
-
-addDecorator(story => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    {story()}
-  </ThemeProvider>
-))
 
 configure(loadStories, module)
